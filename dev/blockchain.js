0a4e9ccd52ef0cd582024c84661d292078a9a4cb
yasirghouri <yasir.ag7@gmail.com>
2022-01-19 21:35:40 +0500

    made a function to createNewBlock

---
+++ dev/blockchain.js
@@ -0,0 +1,24 @@
+function Blockchain() {
+  this.chain = [];
+  this.newTransactions = [];
+}
+
+Blockchain.prototype.createNewBlock = function (
+  nonce,
+  previousBlockHash,
+  hash
+) {
+  const newBlock = {
+    index: this.chain.length + 1,
+    timestamp: Date.now(),
+    transactions: this.newTransactions,
+    nonce,
+    previousBlockHash,
+    hash,
+  };
+
+  this.newTransactions = [];
+  this.chain.push(newBlock);
+
+  return newBlock;
+};
