fd5c1c72f384d16f6de9984f538e4c0e58dae9d8
yasirghouri <yasir.ag7@gmail.com>
2022-01-22 19:34:22 +0500

    created and tested "/mine" endpoint

--- dev/api.js
+++ dev/api.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { v4: uuidv4 } = require("uuid");
 const Blockchain = require("./blockchain");
 const app = express();
 
 const yCoin = new Blockchain();
+const nodeAddress = uuidv4().split("-").join("");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,7 +20,21 @@ app.post("/transaction", function (req, res) {
   res.json({ note: `Transaction will be added in block # ${blockIndex}` });
 });
 app.get("/mine", function (req, res) {
-  res.send("Hello World");
+  const lastBlock = yCoin.getLastBlock();
+  const previousBlockHash = lastBlock["hash"];
+  const currentBlockData = {
+    transactions: yCoin.pendingTransactions,
+    index: lastBlock["index"] + 1,
+  };
+
+  const nonce = yCoin.proofOfWork(previousBlockHash, currentBlockData);
+  const blockHash = yCoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+
+  yCoin.createNewTransaction(12.5, "00", nodeAddress);
+
+  const newBlock = yCoin.createNewBlock(nonce, previousBlockHash, blockHash);
+
+  res.json({ note: "New block mined successfully", block: newBlock });
 });
 
 app.listen(3000, function () {
