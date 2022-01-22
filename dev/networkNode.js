e47c903ba3fe483113b1a14471bc5fc38e126889
yasirghouri <yasir.ag7@gmail.com>
2022-01-22 21:30:05 +0500

    minor fix

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -43,7 +43,9 @@ app.get("/mine", function (req, res) {
 
 app.post("/register-and-broadcast-node", function (req, res) {
   const newNodeUrl = req?.body?.newNodeUrl;
-  if (yCoin.networkNodes.indexOf(newNodeUrl) == -1)
+  const nodeNotAlreadyPresent = yCoin.networkNodes.indexOf(newNodeUrl) == -1;
+  const notCurrentNode = yCoin.currentNodeUrl !== newNodeUrl;
+  if (nodeNotAlreadyPresent && notCurrentNode)
     yCoin.networkNodes.push(newNodeUrl);
 
   const registerNodesPromises = [];
