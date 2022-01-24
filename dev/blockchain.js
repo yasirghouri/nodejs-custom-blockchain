cb2b3cd1ccb75411931a7ccf0805718e4cec4119
yasirghouri <yasir.ag7@gmail.com>
2022-01-24 19:39:09 +0500

    created getTransaction function

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -133,4 +133,20 @@ Blockchain.prototype.getBlock = function (blockHash) {
   return correctBlock;
 };
 
+Blockchain.prototype.getTransaction = function (transactionId) {
+  let correctTransaction = null;
+  let correctBlock = null;
+
+  this.chain.forEach((block) => {
+    block.transactions.forEach((transaction) => {
+      if (transaction.transactionId === transactionId) {
+        correctTransaction = transaction;
+        correctBlock = block;
+      }
+    });
+  });
+
+  return { transaction: correctTransaction, block: correctBlock };
+};
+
 module.exports = Blockchain;
